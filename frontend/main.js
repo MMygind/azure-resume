eec117e73edf9c0cf6a11d484d2c7511f6176f61
MMygind <56871472+MMygind@users.noreply.github.com>
2022-06-20 21:03:50 +0200

    Deployed frontend

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterm.azurewebsites.net/api/GetResumeCounter?code=ZYT_9b8636eS9C1D1hlVBRp6KCY0xoEhz1FicLdB1iLPAzFujJFIFg==';
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 0;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
